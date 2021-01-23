88cbb467bf367af2f124ad5a3786bdc830aeb585
Kamil Kasprzak <kamil.kasprzak@student.wat.edu.pl>
2021-01-23 10:18:15 +0100

    Wykonano zadanie 3.2.4 – echo

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,47 +1,45 @@
 #include <Arduino.h>
-#include <stdio.h>
-
-char rxBuff[32];
-int bytesNo = 0;
-char paramStr[16];
-int paramVal = 0;
-
-void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(4000);
+#include <stdint.h>
+#define FOSC 16000000
+#define BAUD 9600
+#define MY_UBRR (FOSC / 16 / BAUD - 1)
+#define USE_UART_RX_IRQ 1 
+char RecivedChar;
+char Message[]="Hello";
+char*pMessage = Message;
+ 
+void setup() 
+{
   
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-void loop() {
-  bytesNo = Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
-
-  sscanf(rxBuff, "%s %d", paramStr, &paramVal);
-
-  if (bytesNo) {
-    Serial.print("Wykonuje...&quot;
-    if (!strcmp(paramStr, "led&quot) {
-      if (paramVal) {
-        digitalWrite(LED_BUILTIN, HIGH);
-      } else {
-        digitalWrite(LED_BUILTIN, LOW);
+  UBRR0H= (MY_UBRR>>8);
+  UBRR0L = MY_UBRR;
+  UCSR0B|=1<<RXEN0|(1<<TXEN0);
+  UCSR0C |= (1<< UCSZ01)| (1<<UCSZ00);
+  while (*pMessage)
+   {
+     while(!(UCSR0A& (1<<UDRE0)));
+     UDR0 = *pMessage;
+     pMessage++;
       }
-    Serial.println("OK!&quot;
-  } else if (!strcmp(paramStr, "blink&quot) {
-
-  for(int i = 0; i < paramVal; i++) {
-digitalWrite(LED_BUILTIN, HIGH); delay(500);
-digitalWrite(LED_BUILTIN, LOW); delay(500);
-  }
-  Serial.println("OK!&quot;
-  } else {
-    Serial.println("Blad!&quot;
-  }
-  }
-
-  for (int i = 0; i < Serial.available(); i++) {
-    Serial.read();
-  }
-  for (int i = 0; i< sizeof(rxBuff); i++) {
-    rxBuff = 0;
-  }
+  #if (USE_UART_RX_IRQ==1)
+  UCSR0B|=(1<<RXCIE0);
+  #endif
+  
 }
+void loop() 
+{
+  #if (USE_UART_RX_IRQ == 0 )
+  while(!(UCSR0A & (1<<RXC0)));
+  RecivedChar= UDR0;
+  while(!(UCSR0A &(1<<UDRE0)));
+  UDR0 = RecivedChar;  
+  #endif
+  }
+  #if (USE_UART_RX_IRQ ==1)
+  ISR (USART_RX_vect)
+  {
+    RecivedChar = UDR0;
+    UDR0= RecivedChar;
+ 
+  }
+  #endif
