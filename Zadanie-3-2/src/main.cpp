1b9179631f69f9db27eb0f0d8161f8ee0edc6e27
Kamil Kasprzak <kamil.kasprzak@student.wat.edu.pl>
2021-01-31 13:46:36 +0100

    Wykonano zadanie 3.2.3 – konsola wiersza poleceń

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,45 +1,48 @@
 #include <Arduino.h>
-#include <stdint.h>
-#define FOSC 16000000
-#define BAUD 9600
-#define MY_UBRR (FOSC / 16 / BAUD - 1)
-#define USE_UART_RX_IRQ 1 
-char RecivedChar;
-char Message[]="Hello";
-char*pMessage = Message;
- 
-void setup() 
-{
-  
-  UBRR0H= (MY_UBRR>>8);
-  UBRR0L = MY_UBRR;
-  UCSR0B|=1<<RXEN0|(1<<TXEN0);
-  UCSR0C |= (1<< UCSZ01)| (1<<UCSZ00);
-  while (*pMessage)
-   {
-     while(!(UCSR0A& (1<<UDRE0)));
-     UDR0 = *pMessage;
-     pMessage++;
-      }
-  #if (USE_UART_RX_IRQ==1)
-  UCSR0B|=(1<<RXCIE0);
-  #endif
-  
+#include <stdio.h>
+
+char rxBuff[32];
+int bytesNo = 0;
+char paramStr[16];
+int paramVal = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(4000);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
-void loop() 
-{
-  #if (USE_UART_RX_IRQ == 0 )
-  while(!(UCSR0A & (1<<RXC0)));
-  RecivedChar= UDR0;
-  while(!(UCSR0A &(1<<UDRE0)));
-  UDR0 = RecivedChar;  
-  #endif
+
+void loop() {
+ bytesNo = Serial.readBytesUntil('\r' , rxBuff , sizeof(rxBuff) - 1);
+
+  sscanf(rxBuff, "%s %d", paramStr,&paramVal);
+
+  if (bytesNo) {
+    Serial.print("Wykonuje...");
+    if (!strcmp(paramStr, "led")) {
+      if (paramVal) {
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+      Serial.println("OK!");
+    } else if (!strcasecmp(paramStr, "blink")) {
+      for(int i = 0; i <paramVal; i++) {
+        digitalWrite(LED_BUILTIN, HIGH); delay(500);
+        digitalWrite(LED_BUILTIN, LOW); delay(500);
+      }
+      Serial.println("OK!");
+    } else {
+      Serial.println("Blad!");
+    }
   }
-  #if (USE_UART_RX_IRQ ==1)
-  ISR (USART_RX_vect)
-  {
-    RecivedChar = UDR0;
-    UDR0= RecivedChar;
- 
+
+  for (int i = 0; i < Serial.available(); i++) {
+    Serial.read();
   }
-  #endif
+
+for (int i = 0; i < sizeof(rxBuff); i++) {
+  rxBuff[i] = 0;
+}
+}
